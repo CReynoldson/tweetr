107cf1729c166bdefc8cde190bcb870d0f5e550d
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-10-22 00:13:46 +0000

    getTweets and saveTweets both go to mongodb instead of to memory

--- server/index.js
+++ server/index.js
@@ -15,24 +15,6 @@ db.connect((dbInstance) => {
   app.use('/tweets', tweetsApi(dbInstance));
 });
 
-  app.post("/", function(req, res) {
-    if (!req.body.text) {
-      res.status(400);
-      return res.send("{'error': 'invalid request'}\n");
-    }
-
-    const user = req.body.user ? req.body.user : User.generateRandomUser();
-    const tweet = {
-      user: user,
-      content: {
-        text: req.body.text
-      },
-      created_at: Date.now()
-    };
-    db.saveTweet(tweet);
-    return res.send();
-  });
-
 //Setting up connection
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
